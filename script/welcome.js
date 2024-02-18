9ff6ae797ed72a5df6c17546ef72da4e5902c6d1
dzzhar <zharifahdzikra@gmail.com>
2024-02-18 14:21:44 +0700

    add javascript

---
+++ script/welcome.js
@@ -0,0 +1,19 @@
+// external javascript
+
+// method-method BOM (Browser Object Model)
+
+/* 
+method prompt mengembalikan sebuah nilai sesuai dengan inputan user
+nilai apapun yang diinputkan akan diproses dan dikembalikan menjadi data string
+*/
+// const tamu = prompt("Siapakah Anda?");
+
+/*
+method alert ini menerima satu parameter berupa data string yang ditampilkan dengan pop-up
+*/
+// alert("Selamat Datang, " + tamu + "!");
+
+console.log("ini adalah console log");
+console.info("ini adalah console info");
+console.warn("ini adalah console warn");
+console.error("ini adalah console error");
